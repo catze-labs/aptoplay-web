99561f7d0a40671cbbc8b61c7a48733fb5f97ff3
aiiiden <hjjam100@gmail.com>
2023-02-02 15:50:00 +0900

    Add UI of Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,51 @@
+import { useRouter } from "next/router";
+import Button from "./Button";
+import Container from "./Container";
+import Logo from "./Logo";
+
+const Footer = () => {
+  const router = useRouter();
+
+  return (
+    <footer className="bg-black w-full h-[320px] xl:h-[400px] flex flex-col">
+      <Container className="pt-20 pb-20 h-full w-full flex flex-col justify-between">
+        <div>
+          <Logo width={250} />
+          <div className="flex flex-wrap gap-4 xl:gap-10 pt-10 pb-10 xl:pb-0">
+            <Button
+              onClick={() => router.push("/tutorial")}
+              variant="secondary"
+              className="w-fit bg-black"
+            >
+              Tutorial
+            </Button>
+            <Button
+              onClick={() => router.push("/about")}
+              variant="secondary"
+              className="w-fit bg-black"
+            >
+              What is APTOPLAY?
+            </Button>
+            <Button
+              onClick={() => router.push("/login")}
+              variant="secondary"
+              className="w-fit bg-black"
+            >
+              Login
+            </Button>
+            <Button
+              onClick={() => router.push("/signup")}
+              variant="secondary"
+              className="w-fit bg-black"
+            >
+              Sign up
+            </Button>
+          </div>
+        </div>
+        <p className="text-right">© 2022 APTOPLAY. All rights reserved.</p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
